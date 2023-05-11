b75a7f6a0966bd374f2c70164dbf9e961003e645
Bjathneas <bjathneas@gmail.com>
2023-05-11 01:31:10 -0500

    Formatted

--- include/LogCore.h
+++ include/LogCore.h
@@ -4,16 +4,12 @@
 #include <string>
 
 namespace LogCore {
-  enum LOG_LEVEL {
-    DEBUG,
-    WARNING,
-    ERROR
-  };
-  
-  class Logger {
-    public:
-      Logger(std::string name);
-      ~Logger();
+enum LOG_LEVEL { DEBUG, WARNING, ERROR };
+
+class Logger {
+public:
+    Logger(std::string name);
+    ~Logger();
 
     void warning(std::string msg);
     void debug(std::string msg);
@@ -21,11 +17,11 @@ namespace LogCore {
 
     void log(std::string msg, LOG_LEVEL level = LOG_LEVEL::DEBUG);
 
-    private:
-      std::string name;
-      std::fstream log_file;
+private:
+    std::string name;
+    std::fstream log_file;
 
-      bool name_is_valid(std::string name);
-      std::string get_current_time();
-  };
+    bool name_is_valid(std::string name);
+    std::string get_current_time();
+};
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,9 +1,10 @@
-#include  "LogCore.h"
+#include "LogCore.h"
 
-int main() {
-  LogCore::Logger main("main_test");
-  main.debug("this is a debug");
-  main.warning("this is a warning");
-  main.error("this is an error");
-  return EXIT_SUCCESS;
+int main()
+{
+    LogCore::Logger main("main_test");
+    main.debug("this is a debug");
+    main.warning("this is a warning");
+    main.error("this is an error");
+    return EXIT_SUCCESS;
 }
